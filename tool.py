c24ce00a25d202fdbbdc599a78652b64ed8a6569
bantee-sharma <banteesharma9@gmail.com>
2025-04-28 21:31:07 +0530

    Create tool.py

---
+++ tool.py
@@ -0,0 +1,10 @@
+from langchain_community.tools import DuckDuckGoSearchRun
+
+search = DuckDuckGoSearchRun()
+
+'''res = search.invoke("Top news in India")
+
+print(res)'''
+print(search.name)
+print(search.args)
+print(search.description)
