b4a49a5878cfb47b63e6e4f62b98e4e3443dbce3
bantee-sharma <banteesharma9@gmail.com>
2025-05-03 16:55:30 +0530

    Update tool_bindiing.py

--- tool_bindiing.py
+++ tool_bindiing.py
@@ -1,6 +1,8 @@
 from langchain_core.tools import tool
 from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
 
+load_dotenv()
 
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
