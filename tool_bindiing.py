03d197b364ed915b49cf4afa84c16d4da4839d35
bantee-sharma <banteesharma9@gmail.com>
2025-05-03 16:51:07 +0530

    Create tool_bindiing.py

---
+++ tool_bindiing.py
@@ -0,0 +1,21 @@
+from langchain_core.tools import tool
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+@tool
+def multiply(a:int,b:int)->int:
+    '''Multiplying'''
+    return a*b
+
+'''print(multiply.invoke({"a":9,"b":2}))
+
+print(multiply.name)
+print(multiply.description)
+print(multiply.args)'''
+
+#tool binding
+
+llm.bind_tools([multiply])
