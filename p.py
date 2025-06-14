b4f66cdda0678f661a99b373ecd1136b98520269
bantee-sharma <banteesharma9@gmail.com>
2025-06-14 20:59:25 +0530

    Update p.py

--- p.py
+++ p.py
@@ -5,12 +5,17 @@ load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
+chat_history = []
 
 while True:
     query = input("Ask me: ").strip()
+    chat_history.append(query)
     if query.lower() in ["exit","quit"]:
         print("Byee, I am exiting...")
         break
     else:
-        resopnse = llm.invoke(query)
-        print("AI: ",resopnse.content)
+        resopnce = llm.invoke(chat_history)
+        chat_history.append(resopnce)
+        print("AI: ",resopnce.content)
+    
+print(chat_history)
