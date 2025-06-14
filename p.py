8070d7b39f67d2fb8c741cb9459e4712ef786f64
bantee-sharma <banteesharma9@gmail.com>
2025-06-14 20:51:06 +0530

    Update p.py

---
+++ p.py
@@ -0,0 +1,11 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+question = "Who is president of america?"
+response = llm.invoke(question)
+
+print(response)
