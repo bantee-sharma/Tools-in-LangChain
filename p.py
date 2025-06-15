c4a87980a92d4138d0fe4c5a34bb7387b776aeac
bantee-sharma <banteesharma9@gmail.com>
2025-06-15 20:57:33 +0530

    Update p.py

--- p.py
+++ p.py
@@ -1,11 +1,25 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain_experimental.agents import create_csv_agent
+import pandas as pd
+import numpy as np
+import streamlit as st
 
 load_dotenv()
 
-py = PythonREPLTool()
+df = pd.read_excel("dataset netflix.xlsx")
+print(df.head())
 
-query = 'print(2+5)'
-response = py.invoke(query)
-print(response)
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+# csv_agent = create_csv_agent(
+#     llm=llm,
+#     path="dataset netflix.xlsx",
+#     allow_dangerous_code=True,
+#     verbose=True
+# )
+
+# query = "how many rows are there?"
+# response = csv_agent.invoke(query)
+# print(response)
+
