8db2445e845b29da459ae51b9592dd9ea7cd7a54
bantee-sharma <banteesharma9@gmail.com>
2025-06-15 20:46:01 +0530

    Update p.py

--- p.py
+++ p.py
@@ -1,1 +1,11 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_experimental.tools import PythonAstREPLTool,PythonREPLTool
 
+load_dotenv()
+
+py = PythonREPLTool()
+
+query = 'print("2"+"5")'
+response = py.invoke(query)
+print(query)
