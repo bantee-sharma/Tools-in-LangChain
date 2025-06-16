5d84f2cccca34673db335c4abcc4465e93ba0b5f
bantee-sharma <banteesharma9@gmail.com>
2025-06-16 22:38:33 +0530

    Create ot.py

---
+++ ot.py
@@ -0,0 +1,33 @@
+from typing import TypedDict,Optional,Literal,Annotated
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+
+parser = StrOutputParser
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+prompt = PromptTemplate(
+    template="Give the sentiment of the following review:{review}",
+    input_variables=["review"]
+)
+
+query = """Upgraded to the 16 from my 12 and it is a great phone. The Ultramarine Blue looks and feels sooo good. 
+    The photos don't do enough justice to this variant.
+
+    You definitely do not need to upgrade to this if you are having a 14 or a 15, unless Apple Intelligence 
+    is something that you do not want to live without.
+    
+    Camera is great, though not very sure of the Camera Control thing, cuz all that is pretty much available on-screen UI.
+
+    Also, got a great deal on the exchange and bank offer, so zero complaints."""
+
+chain = prompt | llm | parser
+
+response = chain.invoke(query)
+print(response)
+
