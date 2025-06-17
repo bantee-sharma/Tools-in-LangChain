95ac382c57450d883a896fa829353da3a9b4a92f
bantee-sharma <banteesharma9@gmail.com>
2025-06-17 21:11:46 +0530

    Update pydant.py

--- pydant.py
+++ pydant.py
@@ -4,7 +4,3 @@ from dotenv import load_dotenv
 from pydantic import BaseModel, Field
 
 load_dotenv()
-
-model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
-
-llm = model.with_structured_output(Review)
