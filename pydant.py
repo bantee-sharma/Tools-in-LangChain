220016c4ea89eb51c97323876f6c305d11ab7647
bantee-sharma <banteesharma9@gmail.com>
2025-06-17 21:11:38 +0530

    Update pydant.py

--- pydant.py
+++ pydant.py
@@ -7,13 +7,4 @@ load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
-class Review(BaseModel):
-
-    key_themes: list[str] = Field(description="Write down all the key themes discussed in the review in a list")
-    summary: str = Field(description="A brief summary of the review")
-    sentiment: Literal["pos", "neg"] = Field(description="Return sentiment of the review either negative, positive or neutral")
-    pros: Optional[list[str]] = Field(default=None, description="Write down all the pros inside a list")
-    cons: Optional[list[str]] = Field(default=None, description="Write down all the cons inside a list")
-    name: Optional[str] = Field(default=None, description="Write the name of the reviewer")
-
 llm = model.with_structured_output(Review)
