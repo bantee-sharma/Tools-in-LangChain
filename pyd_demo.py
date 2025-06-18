db73b21bcfc90743e9f38fe6067da3f558962f53
bantee-sharma <banteesharma9@gmail.com>
2025-06-18 20:49:00 +0530

    Update pyd_demo.py

--- pyd_demo.py
+++ pyd_demo.py
@@ -1,8 +1,5 @@
 
 
-query = """Upgraded to the 16 from my 12 and it is a great phone. The Ultramarine Blue looks and feels sooo good. 
-    The photos don't do enough justice to this variant.
-
     You definitely do not need to upgrade to this if you are having a 14 or a 15, unless Apple Intelligence 
     is something that you do not want to live without.
     
