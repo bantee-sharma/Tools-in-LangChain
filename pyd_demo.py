c221ac0a8e2e0eb69a3b816727b5c4794192fbf7
bantee-sharma <banteesharma9@gmail.com>
2025-06-18 20:49:07 +0530

    Update pyd_demo.py

--- pyd_demo.py
+++ pyd_demo.py
@@ -1,7 +1,6 @@
 
 
-    You definitely do not need to upgrade to this if you are having a 14 or a 15, unless Apple Intelligence 
-    is something that you do not want to live without.
+
     
     Camera is great, though not very sure of the Camera Control thing, cuz all that is pretty much available on-screen UI.
 
