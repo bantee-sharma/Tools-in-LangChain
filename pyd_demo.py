094f98bba303e49ac2f16e247db6d0780b625cb3
bantee-sharma <banteesharma9@gmail.com>
2025-06-18 20:49:17 +0530

    Update pyd_demo.py

--- pyd_demo.py
+++ pyd_demo.py
@@ -2,9 +2,7 @@
 
 
     
-    Camera is great, though not very sure of the Camera Control thing, cuz all that is pretty much available on-screen UI.
 
-    Also, got a great deal on the exchange and bank offer, so zero complaints."""
 
 
 
